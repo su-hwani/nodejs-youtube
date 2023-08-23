8e442eab40691b8f71fab413260605b2d94ef5d4
wjdtnghks123 <wjdtnghks123@nate.com>
2023-08-23 23:35:08 +0900

    User 모델 구현

---
+++ app/src/models/User.js
@@ -0,0 +1,28 @@
+"use strict"
+const UserStorage = require("./UserStorage")
+
+class User {
+    constructor(body){
+        this.body = body
+    }
+
+    login() {
+        const body = this.body
+        const {id, pw} = UserStorage.getUserInfo(body.id)
+        if (id){
+            if (id ===  body.id && pw === body.pw){
+                return { success: true }
+            }
+            return {
+                success: false,
+                msg: "비밀번호가 틀렸습니다"
+            }
+        }
+        return {
+            success: false,
+            msg: "존재하는 아이디가 없습니다"
+        }
+    }
+}
+
+module.exports = User  
