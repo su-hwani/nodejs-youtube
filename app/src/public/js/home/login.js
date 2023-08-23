f716c612e097284d6ed0024ba942ed0c3fd33cc1
wjdtnghks123 <wjdtnghks123@nate.com>
2023-08-23 20:21:59 +0900

    프론트 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -25,5 +25,13 @@ loginBtn.addEventListener("click", () => {
         body: JSON.stringify(req)
     })
     .then((res) => res.json())
-    .then(console.log)
+    .then((res) => {
+        if (res.success) {
+            location.href = "/"
+        }else {
+            alert(res.msg)
+        }
+    }).catch((err) => {
+        console.err(new Error("로그인 중 에러 발생"))
+    })
 })
