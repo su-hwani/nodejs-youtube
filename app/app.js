39056fbcd9fc416d219cf481e1c0977e5b36b61c
wjdtnghks123 <wjdtnghks123@nate.com>
2023-08-23 19:02:41 +0900

    프런트 단 js 파일 만들기

--- app/app.js
+++ app/app.js
@@ -10,8 +10,9 @@ const home = require("./src/routes/home")
 
 
 // 앱 세팅
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, './src/views'));
 app.set("view engine", "ejs")
+app.use(express.static(path.join(__dirname, 'src/public')));
 
 
 app.use("/",home) // use -> Middleware 를 등록해주는 메서드
