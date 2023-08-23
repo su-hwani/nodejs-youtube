2dc3c93049303ee9be0f3edbd04a18842bbcfa1a
wjdtnghks123 <wjdtnghks123@nate.com>
2023-08-23 19:31:00 +0900

    돔을 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,18 @@
 "use strict"
 
-console.log("hello")
+// <DOM>
+// document.querySelector 는 id 나 tag 등에서 데이터를 가져오는건데, 
+// 여기서 id 와 tag 가 같을 수도 있으니까 id 값으로 데이터를 가져올 때는
+// "#" 을 붙여준다. 
+// ex) button -> 버튼 태그, #button -> id="button" 인 태그 
+const id = document.querySelector("#id")
+const pw = document.querySelector("#pw")
+
+const loginBtn = document.querySelector("button")
+loginBtn.addEventListener("click", () => {
+    const req = {
+        id: id.value,
+        pw: pw.value
+    }
+    console.log(req)
+})
