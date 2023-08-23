46b5aeca39be0fbb32b93a3bbe5651b053f3a72f
wjdtnghks123 <wjdtnghks123@nate.com>
2023-08-23 19:52:07 +0900

    login api 생성

--- app/app.js
+++ app/app.js
@@ -4,6 +4,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const bodyParser = require("body-parser")
 
 // 라우팅
 const home = require("./src/routes/home")
@@ -13,7 +14,9 @@ const home = require("./src/routes/home")
 app.set('views', path.join(__dirname, './src/views'));
 app.set("view engine", "ejs")
 app.use(express.static(path.join(__dirname, 'src/public')));
-
+app.use(bodyParser.json())
+// URL 을 통해 전달되는 데이터에 한글, 공백 등을 처리하지 못하는 문제를 해결
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use("/",home) // use -> Middleware 를 등록해주는 메서드
 
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,20 +1,26 @@
 'use strict';
 const path = require('path')
 
-const home = (req, res) => {
-    const viewPath = path.join(__dirname,'..', '..', 'views', 'home', 'index.ejs');
-    res.render(viewPath)
+const output = {
+    home: (req, res) => {
+        const viewPath = path.join(__dirname,'..', '..', 'views', 'home', 'index.ejs');
+        res.render(viewPath)
+    },
+    login: (req, res) => {
+        const viewPath = path.join(__dirname,'..', '..', 'views', 'home', 'login.ejs');
+        res.render(viewPath)
+    }
 }
 
-const login =  (req, res) => {
-    const viewPath = path.join(__dirname,'..', '..', 'views', 'home', 'login.ejs');
-    res.render(viewPath)
-
+const process = {
+    login: (req, res) => {
+        console.log(req.body)
+    }
 }
 
 module.exports = {
-    home, 
-    login,
+    output,
+    process,
 }
 
 // 이것과 동일하게 적용된다.
